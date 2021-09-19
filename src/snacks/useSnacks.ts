9ee03db17d900d984905128197fca38e6523d92c
Rickard Gimerstedt <rickard@gimerstedt.se>
2021-09-19 17:37:00 +0200

    do not trigger changes unless still mounted

--- src/snacks/useSnacks.ts
+++ src/snacks/useSnacks.ts
@@ -1,19 +1,26 @@
-import { useCallback, useContext } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import { SnackContext } from './SnackContext'
 import { Snack } from './types'
 
 export function useSnacks() {
   const ctx = useContext(SnackContext)
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+    return () => setMounted(false)
+  }, [])
 
   const send = useCallback(
     (snack: Snack) => {
       ctx.setSnacks([...ctx.snacks, snack])
 
       setTimeout(() => {
+        if (!mounted) return
         ctx.setSnacks(ctx.snacks.filter((s) => s === snack))
       }, snack.timeout ?? 3_000)
     },
-    [ctx],
+    [ctx, mounted],
   )
 
   return {
